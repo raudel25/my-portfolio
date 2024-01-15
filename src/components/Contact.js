71dbbc5c2c2164ff95d74db8a1ed6796369e870a
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2024-01-14 21:10:36 -0500

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 // src/components/Contact.js
 
+import { LinkIcon, MailIcon, PhoneIcon } from "@heroicons/react/solid";
 import React from "react";
 
 export default function Contact() {
@@ -16,86 +17,70 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="./main.pdf"
           />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
-              </h2>
-              <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
-              </p>
-            </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                EMAIL
-              </h2>
-              <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
-              </a>
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                PHONE
-              </h2>
-              <p className="leading-relaxed">123-456-7890</p>
-            </div>
-          </div>
+          <a href="./main.pdf" download="RaudelAlejandroGomezMolinaCV.pdf">
+            <button className="relative ] rounded shadow-md text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Download CV
+            </button>
+          </a>
         </div>
-        <form
-          netlify
-          name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-        >
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+        <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-10 font-medium title-font">
+            Contact
           </h2>
-          <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
-          </p>
-          <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
+          <div className="relative mb-2">
+            <h3 className="leading-7 text-sm text-gray-400">Nombre</h3>
+            <p className="text-white">Raudel Alejandro Gómez Molina</p>
           </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label
-              htmlFor="message"
-              className="leading-7 text-sm text-gray-400"
+          <div className="relative mb-2">
+            <h3 className="leading-7 text-sm text-gray-400">Email</h3>
+            <a
+              className="text-white flex items-center"
+              href="mailto:raudel.gmolina@gmail.com"
             >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-            />
+              <MailIcon className="w-4 mr-2" /> raudel.gmolina@gmail.com
+            </a>
           </div>
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          >
-            Submit
-          </button>
-        </form>
+          <div className="relative mb-2">
+            <h3 className="leading-7 text-sm text-gray-400">Phone</h3>
+            <p className="text-white flex items-center">
+              <PhoneIcon className="w-4 mr-2" />
+              +53 59253662
+            </p>
+          </div>
+          <div className="relative mb-2">
+            <h3 className="leading-7 text-sm text-gray-400">Github</h3>
+            <a
+              className="text-white flex items-center"
+              href="https://github.com/raudel25"
+            >
+              <LinkIcon className="w-4 mr-2" />
+              raudel25
+            </a>
+          </div>
+          <div className="relative mb-2">
+            <h3 className="leading-7 text-sm text-gray-400">LinkedIn</h3>
+            <a
+              className="text-white flex items-center"
+              href="https://www.linkedin.com/in/raudel25/"
+            >
+              <LinkIcon className="w-4 mr-2" />
+              raudel25
+            </a>
+          </div>
+          <div className="relative mb-2">
+            <h3 className="leading-7 text-sm text-gray-400">Codeforces</h3>
+            <a
+              className="text-white flex items-center"
+              href="https://codeforces.com/profile/raudel25"
+            >
+              <LinkIcon className="w-4 mr-2" />
+              raudel25
+            </a>
+          </div>
+          {/* Agrega más información de contacto si es necesario */}
+        </div>
       </div>
     </section>
   );
